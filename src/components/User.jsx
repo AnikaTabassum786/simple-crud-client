9a6331c305698ca9598b1e96cbd0ae31b5cce885
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-13 19:53:28 +0600

    Delete

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,12 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 
 const User = ({userPromise}) => {
 
     const initialUsers = use(userPromise);
     console.log(initialUsers)
+
+    
+    const [users,setUsers] = useState(initialUsers)
     
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -28,11 +31,26 @@ const User = ({userPromise}) => {
         .then(data =>{
             console.log('data after creating user in DB',data)
             if(data.insertedId){
+              newUser._id = data.insertedId;
+              const newUsers=[...users,newUser]
+              setUsers(newUsers)
               alert('User added successfully')
               e.target.reset()
             }
         })
     }
+
+    const handleDelete=(id)=>{
+      console.log('Deleted',id)
+      fetch(`http://localhost:3000/users/${id}`, {
+        method:'DELETE'
+      })
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data)
+      })
+    }
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -40,6 +58,17 @@ const User = ({userPromise}) => {
                 <input type="text" name='email' placeholder='email' /> <br />
                 <button type='submit'>Submit</button>
             </form>
+
+            {
+                users?.map((user)=>{
+                    return(
+                        <div key={user._id} >
+                          <p>{user.name}:{user.email}</p>
+                          <button onClick={()=>handleDelete(user._id)} className='btn'>X</button>
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 };
