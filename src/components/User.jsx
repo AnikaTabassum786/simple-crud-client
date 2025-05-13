5ae27ffe4852cd1dad90b5b4f0c9fdc5305a966c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-13 12:53:57 +0600

    first commit

---
+++ src/components/User.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const User = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const name = e.target.name.value
+        const email =e.target.email.value
+        const newUser = {name,email}
+        console.log(newUser)
+
+
+        //create user in DB
+
+        fetch('http://localhost:3000/users',{
+
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(newUser)
+        })
+        .then(res=>res.json())
+        .then(data =>{
+            console.log('data after creating user in DB',data)
+        })
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='name' /> <br />
+                <input type="text" name='email' placeholder='email' /> <br />
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default User;
