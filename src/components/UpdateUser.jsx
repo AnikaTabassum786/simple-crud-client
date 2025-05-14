3f82397962de9e323d26d2b410933994dd6bbccf
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-05-14 13:40:35 +0600

    Update done

--- src/components/UpdateUser.jsx
+++ src/components/UpdateUser.jsx
@@ -7,13 +7,33 @@ const UpdateUser = () => {
     const user = useLoaderData()
     console.log(user)
 
-    const handleUpdate=(e)=>{
+    const handleUpdate = (e) => {
         e.preventDefault();
 
         const name = e.target.name.value
-        const email =e.target.email.value
+        const email = e.target.email.value
 
-        console.log(name,email)
+        // console.log(name,email)
+
+        const updatedUser = { name, email }
+        console.log(updatedUser)
+
+
+
+        fetch(`http://localhost:3000/users/${user._id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedUser)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    console.log('Update Done', data)
+                }
+
+            })
     }
 
     return (
